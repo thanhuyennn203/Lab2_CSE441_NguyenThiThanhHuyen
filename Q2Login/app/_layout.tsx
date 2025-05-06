79e6255e71fee2406fb80893b87d71c637746356
Thanh Huyen <thanhhuyenbb4683@gmail.com>
2025-05-06 12:15:33 +0700

    Add Q2Login folder excluding node_modules

---
+++ Q2Login/app/_layout.tsx
@@ -0,0 +1,32 @@
+import {
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import Styles from "../assets/css/Style.js";
+
+const LoginScreen = () => {
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={Styles.container}>
+        <Text style={Styles.title}>Login</Text>
+
+        <TextInput style={Styles.input} placeholder="Phone" />
+
+        <TextInput
+          style={Styles.input}
+          placeholder="Password"
+          secureTextEntry
+        />
+
+        <TouchableOpacity style={Styles.button}>
+          <Text style={Styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default LoginScreen;
